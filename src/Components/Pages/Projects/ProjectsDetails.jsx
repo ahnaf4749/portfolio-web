46c94f338bea9027f10bb14150ce8012f72e2876
ahnaf4749 <ahnafsagor12@gmail.com>
2022-12-13 23:10:37 +0600

    link added

--- src/Components/Pages/Projects/ProjectsDetails.jsx
+++ src/Components/Pages/Projects/ProjectsDetails.jsx
@@ -4,14 +4,27 @@ import { useLoaderData } from 'react-router-dom';
 const ProjectsDetails = () => {
     const products = useLoaderData()
     console.log(products);
-    const { name, img1, img2, img3, details } = products[0]
+    const { name, img1, img2, img3, details, live_site_link, client_site_link, server_site_link } = products[0]
     return (
         <div>
             <p className='text-center text-4xl font-bold'>Project name : {name}</p>
-            <div className='my-5'>
-                {
-                    details.map(d => <li className='my-2 font-semibold'>{d}</li>)
-                }
+            <div className='flex'>
+                <div className='my-5'>
+                    {
+                        details.map(d => <li className='my-2 font-semibold'>{d}</li>)
+                    }
+                </div>
+                <div className='w-1/2'>
+                    <div className=''>
+                        <a href={live_site_link} target="_blank" rel="noopener noreferrer">Live link</a>
+                    </div>
+                    <div>
+                        <a href={client_site_link} target="_blank" rel="noopener noreferrer">client code link</a>
+                    </div>
+                    <div>
+                        <a href={server_site_link} target="_blank" rel="noopener noreferrer">server code link</a>
+                    </div>
+                </div>
             </div>
             <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 my-10'>
                 <div>
